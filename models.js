afaf3eaf4c79d662319bee0b5fb2361882c2118a
Amri Karisma <amrikarisma@live.com>
2023-08-26 22:31:45 +0700

    add presence

--- models.js
+++ models.js
@@ -9,6 +9,9 @@ const MessageSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
+    presence: {
+        type: String,
+    },
     gift: {
         type: Number,
     },
